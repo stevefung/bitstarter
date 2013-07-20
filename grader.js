d919d3136a4977bf6502f6ec78cb58a766ffb351
Steve <steve.fung@gmail.com>
2013-07-20 01:45:14 +0000

    Update grader

--- grader.js
+++ grader.js
@@ -22,8 +22,10 @@
 */
 
 var fs = require('fs');
+var util = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -31,7 +33,8 @@ var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1);
+        // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -45,7 +48,11 @@ var loadChecks = function(checksfile) {
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+  return checkHtml(cheerioHtmlFile(htmlfile), checksfile);
+};
+
+var checkHtml = function(source, checksfile) {
+    $ = source;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for (var ii in checks) {
@@ -55,20 +62,46 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var outputResult = function(checkJson) {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var buildResponseHandler = function(checksfile) {
+    var handleResponse = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            //handle result
+            outputResult(checkHtml(cheerio.load(result), checksfile));
+        }
+    };
+    return handleResponse;
+}
+
+
 if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var checkJson;
+    if (program.url) {
+        // pull from URL
+        restler.get(program.url).on('complete', buildResponseHandler(program.checks));
+    } else {
+        // use local file
+        checkJson = checkHtmlFile(program.file, program.checks);
+        outputResult(checkJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
